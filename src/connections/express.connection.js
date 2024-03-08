ea8352511f7d3c12e52c2200b55e3fd59b36e239
Pankaj Singh Bedwal <116019841+Pankajbedwal@users.noreply.github.com>
2024-03-08 16:59:49 +0530

    remove unnecessary element

--- src/connections/express.connection.js
+++ src/connections/express.connection.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 class ExpressLoader {
     static init () {
         const app = express();
-        // app.use(express.static('images'));
 
         // Middleware that transforms the raw string of req.body into json
         app.use(bodyParser.urlencoded({ extended: true }));
